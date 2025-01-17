d7b31847a7185bd11f3252a8275d55c3fb5461cb
Gavin Atkinson <gavinatkinson08@gmail.com>
2025-01-16 20:41:58 -0600

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,231 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This file contains an example of a Linear "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode is executed.
+ *
+ * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ *
+ * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ *
+ * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+ * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+ * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+ *
+ * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
+ * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
+ * the direction of all 4 motors (see code below).
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Main")
+public class Main extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        DcMotor leftRear = hardwareMap.get(DcMotor.class, "rearLeft");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "frontRight");
+        DcMotor rightRear = hardwareMap.get(DcMotor.class, "rearRight");
+        DcMotor slide = hardwareMap.get(DcMotor.class, "slide");
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+        Servo clawRotation = hardwareMap.get(Servo.class, "clawRotation");
+        Servo bucketServo = hardwareMap.get(Servo.class, "bucketServo");
+        Servo clawArm = hardwareMap.get(Servo.class, "clawArm");
+        DcMotor climb1 = hardwareMap.get(DcMotor.class, "climb1");
+        DcMotor climb2 = hardwareMap.get(DcMotor.class, "climb2");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftRear.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightRear.setDirection(DcMotor.Direction.FORWARD);
+
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+
+                //Gamepad 1
+
+                if (gamepad1.left_stick_y > 0.5 && gamepad1.left_stick_x > 0.5) {
+                    rightFront.setPower(-0.75);
+                    leftFront.setPower(0);
+                    rightRear.setPower(0);
+                    leftRear.setPower(-0.75);
+                } else if (gamepad1.left_stick_y > 0.5 && gamepad1.left_stick_x < -0.5) {
+                    rightFront.setPower(0);
+                    leftFront.setPower(-0.75);
+                    rightRear.setPower(-0.75);
+                    leftRear.setPower(0);
+                } else if (gamepad1.left_stick_y < -0.5 && gamepad1.left_stick_x < -0.5) {
+                    rightFront.setPower(0.75);
+                    leftFront.setPower(0);
+                    rightRear.setPower(0);
+                    leftRear.setPower(0.75);
+                } else if (gamepad1.left_stick_y < -0.5 && gamepad1.left_stick_x > 0.5) {
+                    rightFront.setPower(0);
+                    leftFront.setPower(0.75);
+                    rightRear.setPower(0.75);
+                    leftRear.setPower(0);
+                } else if (gamepad1.left_stick_y > 0.5) {
+                    rightFront.setPower(-0.75);
+                    leftFront.setPower(-0.75);
+                    rightRear.setPower(-0.75);
+                    leftRear.setPower(-0.75);
+                } else if (gamepad1.left_stick_y < -0.5) {
+                    rightFront.setPower(0.75);
+                    leftFront.setPower(0.75);
+                    rightRear.setPower(0.75);
+                    leftRear.setPower(0.75);
+                } else if (gamepad1.left_stick_x > 0.5) {
+                    rightFront.setPower(-0.75);
+                    leftFront.setPower(0.75);
+                    rightRear.setPower(0.75);
+                    leftRear.setPower(-0.75);
+                } else if (gamepad1.left_stick_x < -0.5) {
+                    rightFront.setPower(0.75);
+                    leftFront.setPower(-0.75);
+                    rightRear.setPower(-0.75);
+                    leftRear.setPower(0.75);
+                } else if (gamepad1.right_bumper) {
+                    rightFront.setPower(-0.75);
+                    leftFront.setPower(0.75);
+                    rightRear.setPower(-0.75);
+                    leftRear.setPower(0.75);
+                } else if (gamepad1.left_bumper) {
+                    rightFront.setPower(0.75);
+                    leftFront.setPower(-0.75);
+                    rightRear.setPower(0.75);
+                    leftRear.setPower(-0.75);
+                } else {
+                    rightFront.setPower(0);
+                    leftFront.setPower(0);
+                    rightRear.setPower(0);
+                    leftRear.setPower(0);
+                }
+                if (gamepad1.right_stick_y > 0.5) {
+                    slide.setPower(-1);
+                } else if (gamepad1.right_stick_y < -0.5) {
+                    slide.setPower(1);
+                } else {
+                    slide.setPower(0.1);
+                }
+
+                if (gamepad1.right_trigger > 0.5) {
+                    climb2.setPower(1);
+                    climb1.setPower(-1);
+                } else if (gamepad1.left_trigger > 0.5) {
+                    climb1.setPower(1);
+                    climb2.setPower(-1);
+                } else {
+                    climb1.setPower(0);
+                }
+
+                //Gamepad 2
+
+                if (gamepad2.a) {
+                    bucketServo.setPosition(0.69);
+                }
+
+                if (gamepad2.b){
+                    bucketServo.setPosition(0.613);
+                } 
+
+                if (gamepad2.left_bumper) {
+                    clawArm.setPosition(0.15);
+                }
+
+                if (gamepad2.right_bumper) {
+                    clawArm.setPosition(0.65);
+                }
+
+                if (gamepad2.dpad_up) {
+                    clawRotation.setPosition(0.61);
+                }
+
+                if (gamepad2.dpad_down) {
+                    clawRotation.setPosition(0.51);
+                }
+
+                if (gamepad2.dpad_left) {
+                    claw.setPosition(0.35);
+                }
+                if (gamepad2.dpad_right) {
+                    claw.setPosition(0.05);
+                }
+                telemetry.addData("Slide Position:", slide.getCurrentPosition());
+                telemetry.addData("Bucket Position:", bucketServo.getPosition());
+                telemetry.addData("Claw Arm Position:", clawArm.getPosition());
+                telemetry.addData("Claw Rotation Position", clawRotation.getPosition());
+                telemetry.addData("Claw", claw.getPosition());
+
+                telemetry.update();
+            }
+        }
+    }
+}
